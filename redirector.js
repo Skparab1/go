b7f04209313b961acd8f1356b219315a288915b4
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-12-31 13:11:34 -0800

    added js

---
+++ redirector.js
@@ -0,0 +1,14 @@
+var loc = window.location.href;
+loc = loc.replace('https://skparab1.github.io/go/','')
+
+if (loc.includes('clarke')){
+    window.open('https://docs.google.com/document/d/12t1be3KzO0hD-UEISfK49zdyhGyRm9AcGRyol9qg6UY/edit',"_self");
+} else if (loc.includes('pascal')){
+    window.open('https://skparab1.github.io/pascals-triangle',"_self");
+} else if (loc.includes('search')){
+    window.open('https://skparab1.github.io/search',"_self");
+} else if (loc.includes('traffic')){
+    window.open('https://skparab1.github.io/traffic_simulator',"_self");
+} else {
+    window.open('https://skparab1.github.io/wrongredirect',"_self")
+}

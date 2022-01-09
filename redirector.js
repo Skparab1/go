59c30a334ec4488ddaf20f8392a5719aea633a74
Shubham <71990977+Skparab1@users.noreply.github.com>
2022-01-08 17:48:57 -0800

    added pusher

--- redirector.js
+++ redirector.js
@@ -1,6 +1,13 @@
+//----------------- PLEASE NO CHANGES BELOW THIS LINE (until i say so)--------------------------
 var loc = window.location.href;
 loc = loc.replace('https://skparab1.github.io/go/','')
 
+keywords = [];
+urls = [];
+//-=---------------- OK YOU CAN MAKE CHANGES NOW--------------------------
+
+
+//-=---------------- PLEASE NO CHANGES BELOW THIS LINE (until i say so)--------------------------
 if (loc.includes('clarke')){
     window.open('https://docs.google.com/document/d/12t1be3KzO0hD-UEISfK49zdyhGyRm9AcGRyol9qg6UY/edit',"_self");
 } else if (loc.includes('pascal')){
@@ -12,3 +19,4 @@ if (loc.includes('clarke')){
 } else {
     window.open('https://skparab1.github.io/wrongredirect',"_self")
 }
+

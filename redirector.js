70cfc3c1e774358fa4f02e0ea559b77929a7674f
Shubham <71990977+Skparab1@users.noreply.github.com>
2022-01-08 18:17:31 -0800

    started algorithm

--- redirector.js
+++ redirector.js
@@ -18,17 +18,12 @@ function r(keyw_set,urw_set){
 r('test','https://www.google.com/search?q=test');
 r('clarke','https://docs.google.com/document/d/12t1be3KzO0hD-UEISfK49zdyhGyRm9AcGRyol9qg6UY/edit');
 r('pascal','https://skparab1.github.io/pascals_triangle')
+r('s','https://skparab1.github.io/search')
+r('traffic','https://skparab1.github.io/traffic_simulator')
 
-//-=---------------- PLEASE NO CHANGES BELOW THIS LINE (until i say so)--------------------------
-if (loc.includes('clarke')){
-    window.open('',"_self");
-} else if (loc.includes('pascal')){
-    window.open('https://skparab1.github.io/pascals_triangle',"_self");
-} else if (loc.includes('search')){
-    window.open('https://skparab1.github.io/search',"_self");
-} else if (loc.includes('traffic')){
-    window.open('https://skparab1.github.io/traffic_simulator',"_self");
-} else {
-    window.open('https://skparab1.github.io/wrongredirect',"_self")
+//------------------ PLEASE NO CHANGES BELOW THIS LINE (until i say so)--------------------------
+
+let index = 0;
+while (index < keywords.length){
+    
 }
-

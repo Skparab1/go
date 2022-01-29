387b305c8c95b132d7f95bba6b04e73c639f03b4
Shubham <71990977+Skparab1@users.noreply.github.com>
2022-01-28 18:02:55 -0800

    back to slower one

--- redirector.js
+++ redirector.js
@@ -1,22 +1,41 @@
+//----------------- PLEASE NO CHANGES BELOW THIS LINE (until i say so)--------------------------
 var loc = window.location.href;
 kew_found = loc.replace('https://skparab1.github.io/go/','')
 
-// r('test','https://www.google.com/search?q=test');
-// r('clarke','https://docs.google.com/document/d/12t1be3KzO0hD-UEISfK49zdyhGyRm9AcGRyol9qg6UY/edit');
-// r('pascal','https://skparab1.github.io/pascals_triangle')
-// r('s','https://skparab1.github.io/search')
-// r('traffic','https://skparab1.github.io/traffic_simulator')
+var keywords = [];
+var urls = [];
+var foundredirect = false;
 
-if (kew_found == 'test'){
-    window.open('https://www.google.com/search?q=test',"_self");
-} else if (kew_found == 'clarke'){
-    window.open('https://docs.google.com/document/d/12t1be3KzO0hD-UEISfK49zdyhGyRm9AcGRyol9qg6UY/edit',"_self");
-} else if (kew_found == 'pascal'){
-    window.open('https://skparab1.github.io/pascals_triangle',"_self");
-} else if (kew_found == 's'){
-    window.open('https://skparab1.github.io/search',"_self");
-} else if (kew_found == 'traffic'){
-    window.open('https://skparab1.github.io/traffic_simulator',"_self");
-} else {
+function r(keyw_set,urw_set){
+    keywords.push(keyw_set);
+    urls.push(urw_set);
+}
+
+//-=---------------- OK YOU CAN MAKE CHANGES NOW--------------------------
+// duplicate those lines and put keywords and urls
+// in the format of r(keyword,url);
+// ok with or without https://
+
+r('test','https://www.google.com/search?q=test');
+r('clarke','https://docs.google.com/document/d/12t1be3KzO0hD-UEISfK49zdyhGyRm9AcGRyol9qg6UY/edit');
+r('pascal','https://skparab1.github.io/pascals_triangle')
+r('s','https://skparab1.github.io/search')
+r('traffic','https://skparab1.github.io/traffic_simulator')
+
+//------------------ PLEASE NO CHANGES BELOW THIS LINE (until i say so)--------------------------
+
+let index = 0;
+while (index < keywords.length){
+    if (keywords[index] == kew_found){
+        let goto = urls[index];
+        goto = goto.replace('https://','');
+        goto = 'https://'+goto;
+        window.open(goto,"_self");
+        foundredirect = true;
+    }
+    index += 1;
+}
+
+if (!foundredirect){
     window.open('https://skparab1.github.io/wrongredirect',"_self");
 }
